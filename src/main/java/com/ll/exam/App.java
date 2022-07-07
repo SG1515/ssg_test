7464755718f398404da713fa445a60dff1535515
SG1515 <sunggu507@naver.com>
2022-07-07 15:40:58 +0900

    등록기능 구현시작

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -14,9 +14,18 @@ public class App {
             System.out.print("명령) ");
             String cmd = br.readLine().trim();
 
+            int n = 1;
 
             //"종료"
             switch ( cmd ){
+                case "등록":
+                    System.out.print("명언 : ");
+                    String wise = br.readLine().trim();
+                    System.out.print("작가 : ");
+                    String writer = br.readLine().trim();
+                    System.out.println(n + "번 명언이 등록되었습니다.");
+                    n++;
+                    break;
                 case "종료":
                     break outer;
 
