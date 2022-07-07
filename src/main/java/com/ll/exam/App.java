be2d010c71ea731697b2422009879ae1053f51f4
SG1515 <sunggu507@naver.com>
2022-07-07 15:30:48 +0900

    명령프롬프트 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,7 +1,30 @@
 package com.ll.exam;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
-    public void run() {
-        System.out.println("=== 명언 SSSG ==");
+    public void run() throws IOException {
+        System.out.println("=== 명언 SSG ==");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        outer:
+        while(true){
+            System.out.print("명령) ");
+            String cmd = br.readLine().trim();
+
+
+            //"종료"
+            switch ( cmd ){
+                case "종료":
+                    break outer;
+
+
+            }
+        }
+
+        br.close();;
+
     }
 }
